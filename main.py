addab25dc2c34efffbd34f6c01e20e365b0f2643
Ben <7ben7ben@gmail.com>
2022-10-28 23:55:43 +0300

    Emails

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import sys
+import csv
 
 def main():
+    path_to_mails = "mails.txt"
+
     promo = sys.argv[1]
     print("Promo Code: " + promo)
 
+    print(read_emails(path_to_mails))
+
+
 if __name__ == '__main__':
     main()
+
+
+def read_emails(path):
+    emails_list = []
+    with open(path) as csvfile:
+        data = csvfile.readlines()
+
+    for i in data:
+        emails_list.append(i.replace("\n", "").replace(" ", ""))
+
+    return emails_list
+
+
