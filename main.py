b197de1449a0901cdfc3f7a1580f93146ce5858d
Ben <7ben7ben@gmail.com>
2022-10-29 00:42:49 +0300

    Pandas

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
 import csv
+import pandas as pd
 
 def main():
     path_to_mails = "mails.txt"
+    emails = []
 
     promo = sys.argv[1]
     print("Promo Code: " + promo)
 
     print(read_emails(path_to_mails))
+    
+    write_to_logs("exmail@gmail.com")
 
 
 if __name__ == '__main__':
@@ -23,5 +27,14 @@ def read_emails(path):
         emails_list.append(i.replace("\n", "").replace(" ", ""))
 
     return emails_list
+
+
+# Use This Func Everytime User Is Ready. Arg: "mail"
+def write_to_logs(completedEmail):
+    print("Email Is Ready To Use: " + completedEmail)
+    emails.append(completedEmail)
+    d = {'Emails': emails}
+    df = pd.DataFrame(data=d)
+    df.to_csv("exfile", sep='\t', encoding='utf-8')
 
 
